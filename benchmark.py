347d20bb5b1b5a4659ce5b5690b93b0dcc7102e8
Nakuziri <xcampos@brooklynsteamcenter.org>
2023-09-19 09:52:45 -0400

    Python Excercise

---
+++ benchmark.py
@@ -0,0 +1,41 @@
+# Excercise 1: Display your name in the console
+print("Xavier Campos")
+
+# Excercise 2: Create a variable called FavoriteShow and set it = to fav show
+FavoriteShow = "Regular Show"
+print(FavoriteShow)
+
+# Excercise 3: Create two variables called height & age and set equal to my own. Then display it in console 
+height = 5.8
+age = 17
+print(height * age)
+
+# Excercise 4: Display "I'm going to break" in the console 
+print("I'm going to break.")
+
+# Excercise 5: Create a list of strings called fav_food and put 3 of your fav foods in it
+fav_food = ['lasagna','shrip','steak']
+
+# Excercise 6: Create a conditional statement that checks if your age is greather than or equal to 21. If true then display you can rent a car otherwise display youre beat
+age = 17
+if age >= 21 :
+    print ("You can rent a car!")
+else :
+    print("you're beat")
+
+# Excercise 7: Ask the user for their name, then display in console "hello <name>"
+Name = input ("What is your name? ")
+print ("Hello " + Name + "!")
+
+# Excercise 8: Create a loop that'll display each item from fav_foods on a seperate line in the console 
+
+for item in(fav_food) :
+    print (item)
+
+# Excercise 9: create a while loop that'll never run
+
+x = 10
+while x == 1: 
+    print('help meeee!!!!')
+
+
