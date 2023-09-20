9dda1e5b87881bf08b72c4e86d212c5c53c3dd0a
Nakuziri <xcampos@brooklynsteamcenter.org>
2023-09-20 10:54:01 -0400

    todays task

---
+++ data_types.py
@@ -0,0 +1,29 @@
+#Task 1:
+float = 9.1 
+print(int(float))
+
+#Task 2: 
+z = int(input("say a number: "))
+if z >= 1 :
+    print("positive")
+    
+elif z == 0 :
+    print("Thats a zero!")
+
+else :
+    print("This is a negative number")
+
+#Task 3: 
+whole = input("give me an interget: ")
+point = input("give me a flot: ")
+add = whole + point
+sub = whole - point
+mult = whole * point 
+div = whole / point
+print(add, sub, mult, div)
+
+#Task 4:
+fruits = {
+ "pear" : 17,
+ "oranges" : 20
+}
