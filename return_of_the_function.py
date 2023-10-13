5154e90f0387d42eb793d8742806d9e116a52986
Nakuziri <xcampos@brooklynsteamcenter.org>
2023-10-13 10:53:55 -0400

    Return for functions

---
+++ return_of_the_function.py
@@ -0,0 +1,37 @@
+"""
+Task 1: Calculate the Square of a Number
+Write a function that takes an integer as an argument and returns its square.
+"""
+def squarerootfun(n):
+    return n**2
+something = squarerootfun(8)
+print(something)
+    
+
+"""
+Task2: Calculate the Area of a Rectangle:
+Write a function that takes the length and width of a rectangle and returns its area.
+"""
+def AreaOfRec(length, width):
+    return length * width
+
+carlos = AreaOfRec(9, 10)
+print(carlos)
+
+"""
+Task 3: Convert Temperature from Celsius to Fahrenheit:
+Write a function that takes a temperature in Celsius and returns 
+the equivalent temperature in Fahrenheit using the correct formula
+"""
+def CelToFar(C) : F = 0
+   return F = C * (9/5) + 32.
+
+convert = CelToFar(10)
+
+
+
+"""
+Task 4: Calculate the Average of Numbers:
+Write a function that takes a list of numbers 
+and returns the average (mean) of those numbers.
+"""
